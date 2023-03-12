3f6a54ccc3d7d5f9141a9809f0deeaeafc990a74
Justin Florentine <justin+github@florentine.us>
2023-03-12 13:56:12 -0400

    trade more dice rolls for entropy bias
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- entropy.py
+++ entropy.py
@@ -8,15 +8,13 @@ class Entropy:
 
     def entropy_add(self, dice) :
         for d in dice:
-            rolled = d-3 #keep higher bits from higher rolls
-            if rolled < 0:
-                continue # skip 1s and 2s being rolled
-            print(f"appending {rolled}")
+            rolled = d-1
             bit1 = rolled & 1
             bit2 = (rolled >> 1) & 1
-            print(f"whos bits are {bit2}{bit1}")
+            bit3 = (rolled >> 2) & 1
             self.runningEntropy.append(bit2)
             self.runningEntropy.append(bit1)
+            self.runningEntropy.append(bit3)
 
     def entropy_full(self):
         return len(self.runningEntropy) >= 1024
