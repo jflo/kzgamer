49d437088d45dcb43891077fd33fe19ba02a5d36
Justin Florentine <justin+github@florentine.us>
2023-03-22 20:45:24 -0400

    fix stupid pathing
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- CameraTest.py
+++ CameraTest.py
@@ -1,6 +1,10 @@
 from PyQt5.QtWidgets import QApplication
 from picamera2.previews.qt import QGlPicamera2
 from picamera2 import Picamera2
+import os
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+os.environ.update({"QT_QPA_PLATFORM_PLUGIN_PATH":"/usr/lib/aarch64-linux-gnu/qt5/plugins/xcbglintegrations/libqxcb-glx-integration.so"})
+
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration())
 app = QApplication([])
