7d1b7a98cc0c3254d99a33a6329f04e548d6942d
Justin Florentine <justin+github@florentine.us>
2023-03-24 20:57:38 -0400

    less circular, lower threshold

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,6 @@ import cv2
 def preprocess(frame):
     frame_blurred = cv2.GaussianBlur(frame, (5,5), 0)
     frame_gray = cv2.cvtColor(frame_blurred, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(frame_gray, 230, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    _, thresh = cv2.threshold(frame_gray, 200, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
     return thresh
