fd293b15f6236f39302a3179b1d9f1339a20d076
Justin Florentine <justin+github@florentine.us>
2023-04-10 14:04:14 -0400

    debug and bit conv
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- entropy.py
+++ entropy.py
@@ -1,7 +1,6 @@
 import os
 
 from bitarray import bitarray
-from bitarray import util
 
 class Entropy:
 
@@ -12,6 +11,7 @@ class Entropy:
             with open("entropy.hex", "r") as f:
                 hex_string = f.read().strip()
             self.running_entropy.frombytes(bytes.fromhex(hex_string))
+            print(f"resuming from {len(self.running_entropy)} bits")
 
     def entropy_add(self, dice) :
         for d in dice:
@@ -31,7 +31,7 @@ class Entropy:
         return len(self.running_entropy) >= self.stop_at
 
     def to_hex_string(self):
-        return bitarray.util.ba2hex(self.truncate_to_multiple_of_4(self.running_entropy))
+        return self.running_entropy.tobytes().hex()
 
     def bits_collected(self):
         return len(self.running_entropy)
