423affa56df2f806e25db0b480f21759c784f4ab
Justin Florentine <justin@florentine.us>
2023-04-10 10:58:10 -0400

    switch to radios

--- MainWidget.py
+++ MainWidget.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout, \
-    QPushButton, QTextEdit, QLabel, QSizePolicy, QSlider, QHBoxLayout
+    QPushButton, QRadioButton, QTextEdit, QLabel, QSizePolicy, QSlider, QHBoxLayout
 from PyQt5.QtGui import QFont
 from KZGamer import KZGamerThread
 
@@ -34,26 +34,20 @@ class MainWindow(QWidget):
 
         main_layout.addWidget(self.last_roll_display)
 
-        # Slider for input control
-        self.mode_slider = QSlider(QtCore.Qt.Horizontal)
-        self.mode_slider.setMinimum(0)
-        self.mode_slider.setMaximum(1)
-        self.mode_slider.setTickInterval(1)
-        self.mode_slider.setValue(1)
+         # Add the radio buttons for the mode input
+        mode_label = QLabel("Mode:")
+        mode_label.setFont(QFont("Helvetica", 14))
+        self.hit_damage_radio = QRadioButton("Hit/Damage")
+        self.morale_radio = QRadioButton("Morale")
+        self.hit_damage_radio.toggled.connect(self.hit_damage_mode)
+        self.morale_radio.toggled.connect(self.morale_mode)
 
-        # Labels for slider
-        slider_labels_layout = QHBoxLayout()
-        self.hit_label = QLabel("Hit/Damage")
-        self.hit_label.setAlignment(QtCore.Qt.AlignRight)
-        self.morale_label = QLabel("Morale")
-        self.morale_label.setAlignment(QtCore.Qt.AlignLeft)
+        mode_layout = QHBoxLayout()
+        mode_layout.addWidget(mode_label)
+        mode_layout.addWidget(self.hit_damage_radio)
+        mode_layout.addWidget(self.morale_radio)
 
-        slider_labels_layout.addWidget(self.hit_label)
-        slider_labels_layout.addWidget(self.mode_slider)
-        slider_labels_layout.addWidget(self.morale_label)
-        self.last_roll_display.setText("Ready for rollin")
-
-        main_layout.addLayout(slider_labels_layout)
+        main_layout.addLayout(mode_layout)
 
         button_layout = QGridLayout()
         button_layout.setSpacing(0)
@@ -71,12 +65,9 @@ class MainWindow(QWidget):
 
         button_grid = QWidget()
         button_grid.setLayout(button_layout)
-        self.mode_slider.setFixedWidth(int(button_grid.width()/3))
-        self.mode_slider.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.morale_radio.setChecked(True) # Set Hit/Damage mode as the default
         main_layout.addWidget(button_grid)
         self.setLayout(main_layout)
-
-        self.mode_slider.sliderReleased.connect(self.slider_released_handler)
 
     def button_click_handler(self):
         sender = self.sender()
@@ -89,10 +80,8 @@ class MainWindow(QWidget):
         self.last_roll_display.setText(message)
         self.log_pane.append(f"[{QtCore.QTime.currentTime().toString('hh:mm:ss')}] {message}")
 
-    def slider_released_handler(self):
-        if self.mode_slider.value() == 0:
-            self.hit_label.setStyleSheet("font-weight: bold")
-            self.morale_label.setStyleSheet("")
+    def hit_damage_mode(self, checked):
+        if checked:
             self.log_pane.append("Setting Hit/Damage mode")
             for i in range(1, 13):
                 button = self.findChild(QPushButton, str(i))
@@ -101,9 +90,9 @@ class MainWindow(QWidget):
                 else:
                     button.setEnabled(False)
             self.kzgamer_thread.mode_selected("Hit/Damage")
-        else:
-            self.morale_label.setStyleSheet("font-weight: bold")
-            self.hit_label.setStyleSheet("")
+            
+    def morale_mode(self, checked):
+        if checked:
             self.log_pane.append("Setting Morale mode")
             for i in range(1, 13):
                 button = self.findChild(QPushButton, str(i))
